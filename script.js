9a950472763ecb00dffe590ce916e3773fa223bc
jestinskaria <44378078+jestinskaria@users.noreply.github.com>
2019-01-14 16:55:13 -0500

    javscript

---
+++ script.js
@@ -0,0 +1,18 @@
+// Name: Jestin Skaria
+// Date: January 14,2019
+// Student: c0745078
+//Course: 
+
+let countDown;
+let timeIntervel;
+const endDate = document.querySelector('input[name ="endDate"]');
+console.log("hey jest");
+console.log(endDate.value)
+
+const clock = document.querySelector('clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+
+
